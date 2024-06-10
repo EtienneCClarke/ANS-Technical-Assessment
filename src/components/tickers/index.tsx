7d3829c3cbd4c686c4babbc70ee705878cd078b0
Etienne <34932238+EtienneCClarke@users.noreply.github.com>
2024-06-10 15:13:46 +0100

    Displayed ticker data to page

--- src/components/tickers/index.tsx
+++ src/components/tickers/index.tsx
@@ -1,10 +1,17 @@
 import { useEffect } from "react";
 import { useTickerData } from "../../hooks/blockchainApi";
 import css from "./tickers.module.css";
+import { useCurrencies } from "../../hooks/useCurrencies";
 
 const Tickers = () => {
 
     const { data, error, loading } = useTickerData();
+    const {currencies, setCurrencies} = useCurrencies();
+
+    useEffect(() => {
+        if(!data) return;
+        setCurrencies(Object.keys(data));
+    }, [data])
 
     if(loading || error) {
         return (
@@ -18,9 +25,31 @@ const Tickers = () => {
     if(data) {
         return (
             <div className={css.tickers__container}>
-                {Object.keys(data).map(ticker => {
+                <div className={css.ticker_heading}>
+                    <h2 className={css.grid_heading}>Symbol</h2>
+                    <h2 className={css.grid_heading}>15m</h2>
+                    <h2 className={css.grid_heading}>Last</h2>
+                    <h2 className={css.grid_heading}>Buy</h2>
+                    <h2 className={css.grid_heading}>Sell</h2>
+                </div>
+                {currencies.map((currency: string) => {
                     return (
-                        <p>{data[ticker]?.symbol}</p>
+                        <div
+                            className={css.ticker_card__container}
+                            key={currency}
+                        >
+                            <p data-cell>
+                                {(data as any)[currency]?.symbol === currency ?
+                                    <>{currency}</>
+                                    :
+                                    <>{`${(data as any)[currency]?.symbol} | ${currency}`}</>
+                                }
+                            </p>
+                            <p data-cell>{(data as any)[currency]["15m"]}</p>
+                            <p data-cell>{(data as any)[currency]?.last}</p>
+                            <p data-cell>{(data as any)[currency]?.buy}</p>
+                            <p data-cell>{(data as any)[currency]?.sell}</p>
+                        </div>
                     )
                 })}
             </div>
