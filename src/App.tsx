b63b927d672227e6e9af81c2f2b2e56982d36e50
Etienne <34932238+EtienneCClarke@users.noreply.github.com>
2024-06-10 14:57:41 +0100

    Added tab navigation to toggle between component views

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import { useEffect } from "react";
-import { BlockchainApiClient } from "./lib/blockchainApiClient";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/tabs";
 
 function App() {
 
-  const client = new BlockchainApiClient();
-
-  useEffect(() => {
-    client.exchangeToBTC("USD", 20).then(res => {
-      console.log(res);
-    })
-  }, [])
-
   return (
-    <>
-    </>
+	<section>
+		<Tabs defaultValue="tickers">
+		<TabsList>
+			<TabsTrigger value="tickers">Tickers</TabsTrigger>
+			<TabsTrigger value="exchange">Exchange</TabsTrigger>
+		</TabsList>
+		<TabsContent value="tickers">
+			<h1>Tickers</h1>
+		</TabsContent>
+		<TabsContent value="exchange">
+			<h1>Exchange</h1>
+		</TabsContent>
+		</Tabs>
+	</section>
   )
 }
 
