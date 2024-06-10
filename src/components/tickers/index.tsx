1584ebf32b782fb413e717651b015329b7eb3fe3
Etienne <34932238+EtienneCClarke@users.noreply.github.com>
2024-06-10 14:52:11 +0100

    Started creating tickers component

---
+++ src/components/tickers/index.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { useTickerData } from "../../hooks/blockchainApi";
+import css from "./tickers.module.css";
+
+const Tickers = () => {
+
+    const { data, error, loading } = useTickerData();
+
+    if(loading || error) {
+        return (
+            <div>
+                {loading && <p>Loading...</p>}
+                {error && <p>{error}</p>}
+            </div>
+        )
+    }
+
+    if(data) {
+        return (
+            <div className={css.tickers__container}>
+            </div>
+        )
+    }
+
+}
